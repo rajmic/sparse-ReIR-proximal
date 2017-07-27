a1e0659035a27fc8cd9a7d5e2ff97bc9d3e84518
koldovsk <30461178+koldovsk@users.noreply.github.com>
2017-07-27 09:59:17 +0200

    Delete subsampled_fft.m

--- subsampled_fft.m
+++
@@ -1,6 +0,0 @@
-function y = subsampled_fft(x,indexes)
-    % performs fft and subsamples it
-    % the FFT is normalized (to become unitary)
-    aux = fft(x) / sqrt(length(x));
-    y = aux(indexes);
-end
